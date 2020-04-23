479a35d6ac38045b0a2a0d51b149d1b5d810e4d1
Kyle Berner <kyleberner14@yahoo.com>
2020-04-22 22:18:21 -0700

    Need to finish api/challenges

---
+++ models/challenges.js
@@ -0,0 +1,18 @@
+module.exports = function(sequelize, DataTypes) {
+  var Challenge = sequelize.define("Challenges", {
+    // The email cannot be null, and must be a proper email before creation
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    task: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    Increment: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+  return Challenge;
+};
