b6d478f0cc80d15b6ce2ce15fb57b01fd1a96658
Ruben Burrell <rubenburrell828@gmail.com>
2020-04-23 14:01:00 -0700

    added workout table to members.js

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,7 @@
 const workoutForm = $("#workout-form");
 const dropdownSelect = $("#workout");
 const workoutTable = $("#table");
-$(document).ready(function() {
+$(document).ready(function () {
   $(workoutForm).on("submit", function handleFormSubmit(event) {
     event.preventDefault();
     var challenge = {
@@ -49,16 +49,36 @@ $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
 
-  $.get("/api/user_data").then(function(data) {
+  $.get("/api/user_data").then(function (data) {
     $(".member-name").text(data.email);
   });
 });
 
 function renderTable(days) {
-  $.each(days, function(i) {
+  $.each(days, function (i) {
     let li = $("<li/>")
       .addClass("day-item w3-panel w3-card")
       .attr("id", i)
       .appendTo(workoutTable);
+    let divRow = $("<div/>")
+      .addClass("w3-row")
+      .appendTo(li);
+    let divCol1 = $("<div/>")
+      .addClass("w3-col s3 w3-green w3-center")
+      .appendTo(divRow);
+    $("<p>Text</p>").appendTo(divCol1);
+    let divCol2 = $("<div/>")
+      .addClass("w3-col s3 w3-green w3-center")
+      .appendTo(divRow);
+    $("<p>Text</p>").appendTo(divCol2);
+    let divCol3 = $("<div/>")
+      .addClass("w3-col s3 w3-green w3-center")
+      .appendTo(divRow);
+    $("<p>Text</p>").appendTo(divCol3);
+    let divCol4 = $("<div/>")
+      .addClass("w3-col s3 w3-green w3-center")
+      .appendTo(divRow);
+    $("<p>Text</p>").appendTo(divCol4);
+
   })
 }
